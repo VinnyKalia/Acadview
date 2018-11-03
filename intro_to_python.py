2531ef07748d66c03f45ca193395ef39f14108c2
VinnyKalia <vinny16791@gmail.com>
2018-11-04 01:11:17 +0530

    intro to python

---
+++ intro_to_python.py
@@ -0,0 +1,28 @@
+# Ques1
+print ("Hello Vinny... Get well soon")
+
+# ques2
+print("Acad"+"View")
+
+#ques3
+x = int(input("enter no. 1: "))
+y = int(input("enter no. 2: "))
+z = int(input("enter no. 3: "))
+print("no. 1 = "+str(x))
+print("no. 2 = "+str(y))
+print("no. 3 = "+str(z))
+
+#ques4
+print("Let's get started")
+
+#ques5
+s="Acadview" 
+course="Python" 
+fees=5000 
+print("%s fees in %s is %d" %(course, s, fees))
+
+#ques6
+r = float(input("enter radius: ")
+pi = 3.14
+a = pi*r*r
+print("Area of Circle: %f"%(a))
